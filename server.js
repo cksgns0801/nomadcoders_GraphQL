51bc14aa36ea3985a6c678c8eab6b94327b175e1
L10H <cksgns080101@gmail.com>
2022-09-27 18:52:23 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ import {ApolloServer, gql} from "apollo-server"
 
 const typeDefs = gql`
 
+    type Query {
+        text : String
+        hello : String
+    }
+
+`;
 
 
-`
-
-
-const server = new ApolloServer({})
+const server = new ApolloServer({typeDefs});
 
 server.listen().then(({url}) => {
     console.log(`Running on ${url}`);
